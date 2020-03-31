cb9a5e2ce418c20d0fcb070af5e2a4c22305addb
Marek Ištvánek <istvanek@hasam.cz>
2020-03-31 16:16:10 +0200

    TableData uses BatchSize insted of DefaultBatchSize

--- ETLBox/src/Toolbox/DataFlow/DBDestination.cs
+++ ETLBox/src/Toolbox/DataFlow/DBDestination.cs
@@ -87,7 +87,7 @@ namespace ALE.ETLBox.DataFlow
 
         private TableData<TInput> CreateTableDataObject(ref TInput[] data)
         {
-            TableData<TInput> td = new TableData<TInput>(DestinationTableDefinition, DefaultBatchSize);
+            TableData<TInput> td = new TableData<TInput>(DestinationTableDefinition, BatchSize);
             td.Rows = ConvertRows(ref data);
             if (TypeInfo.IsDynamic && data.Length > 0)
                 foreach (var column in (IDictionary<string, object>)data[0])
